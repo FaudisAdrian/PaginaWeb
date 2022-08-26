fb899d282577b4d63fd62ab109aa8edefa3ef6b2
FaudisAdrian <111906918+FaudisAdrian@users.noreply.github.com>
2022-08-26 16:50:09 -0500

    Delete conect.php

--- conect.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "/ssl/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, "ecoturismo-azure-website.mysql.database.azure.com", "administrador", "Military2000", "ecoturismo", 3306, MYSQLI_CLIENT_SSL);?>
